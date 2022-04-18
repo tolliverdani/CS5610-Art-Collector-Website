3f25b8789de69153c8a2a3e40270203b7f065714
tolliverdanielle <tolliver.da@northeastern.edu>
2022-04-18 00:46:43 -0400

    updates to the nav

--- src/components/NavigationTopMenu/LoginModal/index.js
+++ src/components/NavigationTopMenu/LoginModal/index.js
@@ -15,37 +15,36 @@ const LogInModal = () => {
             </Button>
 
             <Modal show={set} onHide={handleClose}>
-                <Modal.Header closeButton>
+                <Modal.Header closeButton className={`border-0`}>
                     <div className={'container text-center'}>
                         <Modal.Title>Welcome back!</Modal.Title>
                     </div>
                 </Modal.Header>
 
                 <Modal.Body>
-                    <form>
+                    <form className={`container`}>
                         <div className="form-group row mb-4">
                             <label htmlFor="InputEmail" className="col-sm-2 col-form-label">Email</label>
                             <div className="col-sm-10">
-                                <input type="email" className="form-control rounded-pill" id="InputEmail"
+                                <input type="email" className="form-control rounded-pill bg-light border-0 shadow-none" id="InputEmail"
                                        placeholder="email@domain.com"/>
-                                <small id="emailHelp" className="form-text text-muted">
-                                    We'll never share your email with anyone else.
-                                </small>
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="InputPassword" className="col-sm-2 col-form-label">Password</label>
                             <div className="col-sm-10">
-                                <input type="password" className="form-control rounded-pill" id="InputPassword"
+                                <input type="password" className="form-control rounded-pill bg-light border-0 shadow-none" id="InputPassword"
                                        placeholder="Password"/>
                             </div>
                         </div>
                     </form>
                 </Modal.Body>
 
-                <Modal.Footer>
+                <Modal.Footer className={`border-0`}>
                     <div className={'container text-center'}>
-                        <button className={'btn btn-primary bg-highlight rounded-pill border-0 mb-2'}>Login</button>
+                        <button className={'btn btn-primary bg-primary rounded-pill border-0 mb-2'}>
+                            Login
+                        </button>
                         <div className={'row'}>
                             <a className={'text-center small text-muted'} href={'/#'}>Forgot your password?</a>
                         </div>
--- src/components/NavigationTopMenu/SignUpModal/index.js
+++ src/components/NavigationTopMenu/SignUpModal/index.js
@@ -5,8 +5,10 @@ import {Modal, Button, InputGroup, Form} from 'react-bootstrap';
 
 const SignUpModal = () => {
     const [set, setShow] = useState(false);
+
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
     return (
         <>
             <Button className="btn btn-secondary rounded-pill border-0 m-1" onClick={handleShow}>
@@ -14,44 +16,54 @@ const SignUpModal = () => {
             </Button>
 
             <Modal show={set} onHide={handleClose}>
-                <Modal.Header closeButton>
+                <Modal.Header closeButton className={`border-0`}>
                     <div className={'container text-center'}>
                         <Modal.Title>Register</Modal.Title>
                     </div>
                 </Modal.Header>
 
                 <Modal.Body>
-                    <form>
+                    <form className={`container`}>
                         <div className="form-group row mb-4">
                             <label htmlFor="InputEmail" className="col-sm-2 col-form-label">Email</label>
                             <div className="col-sm-10">
-                                <input type="email" className="form-control rounded-pill" id="InputEmail"
+                                <input type="email" className="form-control rounded-pill bg-light border-0 shadow-none" id="InputEmail"
                                        placeholder="email@domain.com"/>
-                                <small id="emailHelp" className="form-text text-muted">
-                                    We'll never share your email with anyone else.
+                                <small id="emailHelp" className="form-text text-muted ps-2">
+                                    We'll never share your email with anyone else
                                 </small>
                             </div>
                         </div>
                         <div className="form-group row mb-4">
                             <label htmlFor="InputUsername" className="col-sm-2 col-form-label">Username</label>
                             <div className="col-sm-10">
-                                <input type="text" className="form-control rounded-pill" id="InputUsername"
+                                <input type="text" className="form-control rounded-pill bg-light border-0 shadow-none" id="InputUsername"
                                        placeholder="JohnDoe123"/>
                             </div>
                         </div>
-                        <div className="form-group row">
+
+                        <div className="form-group row mb-3">
                             <label htmlFor="InputPassword" className="col-sm-2 col-form-label">Password</label>
                             <div className="col-sm-10">
-                                <input type="password" className="form-control rounded-pill" id="InputPassword"
+                                <input type="password" className="form-control rounded-pill bg-light border-0 shadow-none" id="InputPassword"
+                                       placeholder="Password"/>
+                            </div>
+                        </div>
+
+                        <div className="form-group row align-items-center">
+                            <label htmlFor="InputPassword2" className="col-sm-2 col-form-label">Confirm Password</label>
+                            <div className="col-sm-10">
+                                <input type="password" className="form-control rounded-pill bg-light border-0 shadow-none" id="InputPassword2"
                                        placeholder="Password"/>
                             </div>
                         </div>
                     </form>
                 </Modal.Body>
 
-                <Modal.Footer>
+                <Modal.Footer className={`border-0`}>
                     <div className={'container text-center'}>
-                        <button className={'btn btn-primary bg-primary rounded-pill border-0 m-2'}>
+                        <button className={'btn btn-primary bg-primary rounded-pill border-0 m-2'}
+                        >
                             Register
                         </button>
                         <button className={'btn btn-primary bg-danger rounded-pill border-0 m-2'}
