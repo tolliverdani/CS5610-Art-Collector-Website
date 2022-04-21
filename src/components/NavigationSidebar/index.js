7082c75aa5afc4e3a244b3af1ad22999ff7af4b3
tolliverdanielle <tolliver.da@northeastern.edu>
2022-04-21 00:52:31 -0400

    playing with nav sidebar

--- src/components/NavigationSidebar/index.js
+++ src/components/NavigationSidebar/index.js
@@ -24,34 +24,35 @@ const NavigationSidebar =
                 <ul className={`list-group`}>
                     <Link to="/home"
                           className={`list-group-item list-group-item-action text-center text-lg-start border-0 p-1 ${active === 'home' ? 'active' : ''}`}>
-                        <i className="fa fa-light fa-fire"/>
+                        <i className="fa fa-light fa-fire ps-lg-2"/>
                         <span className={`d-none d-lg-inline ps-2`}>Popular</span>
                     </Link>
                     <Link to="/new-releases"
                           className={`list-group-item list-group-item-action text-center text-lg-start border-0 p-1 ${active === 'new-releases' ? 'active' : ''}`}>
-                        <i className={`fa fa-regular fa-flag`}/>
+                        <i className={`fa fa-regular fa-flag ps-lg-2`}/>
                         <span className={`d-none d-lg-inline ps-2`}>New Releases</span>
                     </Link>
                     <Link to="/new-artists"
                           className={`list-group-item list-group-item-action text-center text-lg-start border-0 p-1 ${active === 'new-artists' ? 'active' : ''}`}>
-                        <i className={`fa fa-solid fa-user-plus`}/>
+                        <i className={`fa fa-solid fa-user-plus ps-lg-2`}/>
                         <span className={`d-none d-lg-inline ps-2`}>New Artists</span>
                     </Link>
                     <Link to="/new-auctions"
                           className={`list-group-item list-group-item-action text-center text-lg-start border-0 p-1 ${active === 'new-auctions' ? 'active' : ''}`}>
-                        <i className={`fa fa-solid fa-chart-bar`}/>
+                        <i className={`fa fa-solid fa-chart-bar ps-lg-2`}/>
                         <span className={`d-none d-lg-inline ps-2`}>New Auctions</span>
                     </Link>
 
                     <SecureContent>
                         <Link to="/profile"
                               className={`list-group-item list-group-item-action text-center text-lg-start border-0 p-1`}>
-                            <i className="fa fa-grip-horizontal"/>
+                            <i className="fa fa-grip-horizontal ps-lg-2"/>
                             <span className={`d-none d-lg-inline ps-2`}>My Collection</span>
                         </Link>
-                        <Link to={"/"}
+                        {/* TODO: need to fix this link */}
+                        <Link to={"/home"}
                               className={`list-group-item list-group-item-action text-center text-lg-start border-0 p-1`}>
-                            <i className="fa fa-arrow-alt-circle-right text-danger"/>
+                            <i className="fa fa-arrow-alt-circle-right text-danger ps-lg-2"/>
                             <span className={`d-none d-lg-inline ps-2`}>Logout</span>
                         </Link>
                     </SecureContent>
