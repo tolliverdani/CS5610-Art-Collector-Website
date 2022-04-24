9fb8527865d80974ddc6c45d3610bce43b61522c
tolliverdanielle <tolliver.da@northeastern.edu>
2022-04-24 00:54:01 -0400

    bigger logo

--- src/components/NavigationTopMenu/index.js
+++ src/components/NavigationTopMenu/index.js
@@ -6,16 +6,19 @@ import SignUpModal from "./SignUpModal";
 import LogInModal from "./LoginModal";
 import SecureContent from "../Security/secure-content";
 import UnsecuredContent from "../Security/unsecured-content";
-import LOGO from "../../_images/artbubblelogo.png"
+import SMALL_LOGO from "../../_images/artbubblelogo.png"
+import FULL_LOGO from "../../_images/artbubblelogo2.png"
+
 
 const NavigationTopMenu = () => {
     return (
         <div className={'navbar sticky-top navbar-dark bg-primary p-2 p-0'}>
+            {/* TODO: need to pick logo based on screen size */}
             <div className={'col-2 col-lg-2 col-xl-3 text-center'}>
                 <a className={'navbar-brand'} href={"/home"}>
                     <img className={``}
-                        src={LOGO} alt={"artbubble logo"}
-                    height={"50px"}/>
+                         src={FULL_LOGO} alt={"artbubble logo"}
+                         height={"50px"}/>
                 </a>
             </div>
 
