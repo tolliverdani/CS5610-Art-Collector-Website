69c82f19db3f090266f704de4a99bc5b643dc0b4
tolliverdanielle <tolliver.da@northeastern.edu>
2022-04-24 00:40:22 -0400

    simple logo

--- src/components/NavigationTopMenu/index.js
+++ src/components/NavigationTopMenu/index.js
@@ -6,12 +6,17 @@ import SignUpModal from "./SignUpModal";
 import LogInModal from "./LoginModal";
 import SecureContent from "../Security/secure-content";
 import UnsecuredContent from "../Security/unsecured-content";
+import LOGO from "../../_images/artbubblelogo.png"
 
 const NavigationTopMenu = () => {
     return (
         <div className={'navbar sticky-top navbar-dark bg-primary p-2 p-0'}>
             <div className={'col-2 col-lg-2 col-xl-3 text-center'}>
-                <a className={'navbar-brand'} href={"/home"}>ART BUBBLE</a>
+                <a className={'navbar-brand'} href={"/home"}>
+                    <img className={``}
+                        src={LOGO} alt={"artbubble logo"}
+                    height={"50px"}/>
+                </a>
             </div>
 
             <div className={'col-8 col-lg-8 col-xl-6'}>
