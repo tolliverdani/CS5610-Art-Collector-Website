eb43bc0b918bf3c91fe38e7ae5d34b859226225f
nziegler87 <njz5001@gmail.com>
2022-10-10 21:54:35 -0400

    setting reload for page

--- src/components/NavigationTopMenu/HamburgerMenu/index.js
+++ src/components/NavigationTopMenu/HamburgerMenu/index.js
@@ -13,6 +13,7 @@ const HamburgerMenu = () => {
         try {
             await logout(dispatch)
             navigate('/home')
+            window.location.reload(false);
         } catch (e) {
             alert("Ut oh! We were unable to log out of this account")
         }
